d7cfa335fbec453d9c0b31fe3167c088d9eafcf6
Trey C <treyton.cowell@ontariotechu.net>
2021-03-20 15:28:35 -0400

    Sound Game Push 1

---
+++ Assets/PhysicalAssets/PlaneMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+
+    public float planeMoveSpeed = 2.0f;
+
+    public Rigidbody rb;
+
+    Vector3 planeMovement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            planeMovement.y--;
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            planeMovement.y = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            planeMovement.y++;
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            planeMovement.y = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + (planeMovement * -1) * planeMoveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/PhysicalAssets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5.0f;
+
+    public Rigidbody rb;
+
+    Vector3 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + (movement * -1) * moveSpeed * Time.fixedDeltaTime);
+    }
+}
