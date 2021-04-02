83e2c264472c9948212bad974f0d490245f2129a
Trey C <treyton.cowell@ontariotechu.net>
2021-04-02 17:02:08 -0400

    Same sound on box hit

--- Assets/PhysicalAssets/Scripts/PlayerMovement.cs
+++ Assets/PhysicalAssets/Scripts/PlayerMovement.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private AudioSource A1_3;//Map.3 //A1 Audio Clip Plays on each square.
 
     public float moveSpeed = 5.0f;
 
     public Rigidbody rb;
 
     Vector3 movement;
+
+    void Start()
+    {
+        A1_3 = GameObject.FindObjectOfType<AudioSource>();
+    }
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("Hit");
+        A1_3.Play();//A1 Audio Clip Plays on each square.
+    }
 
     // Update is called once per frame
     void Update()
